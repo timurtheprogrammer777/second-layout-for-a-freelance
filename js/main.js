3cff5aa1442773cf0940ed001d636e279ad23656
timur1243 <86830091+timur1243@users.noreply.github.com>
2022-03-11 18:27:55 +0300

    slider in process

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $(function () {
+
+
+//     $('.examples__content').slick({
+//       infinite: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       arrows: true,
+      
+  
+//     });
+  
+ 
+  
+  
+//   });
